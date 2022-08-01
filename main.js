251af9f60de209155c6664bc19f9e68c3e60b48a
Swetha-Rajasekaran <swetha.rajasekaran10@gmail.com>
2022-08-01 15:40:51 +0530

    Update: increment & decrement functions

--- main.js
+++ main.js
@@ -71,7 +71,8 @@ let increment = (id) => {
   } else {
     search.item += 1;
   }
-  console.log(basket);
+
+  update(selectedItem.id);
 };
 let decrement = (id) => {
   let selectedItem = id;
@@ -82,5 +83,15 @@ let decrement = (id) => {
     search.item -= 1;
   }
   console.log(basket);
+  update(selectedItem.id);
 };
-let update = () => {};
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
