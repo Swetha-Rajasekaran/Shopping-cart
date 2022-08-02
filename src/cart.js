49301ebd70a6793b2c709f6b4383cc0c2fac5e25
Swetha-Rajasekaran <swetha.rajasekaran10@gmail.com>
2022-08-02 10:33:43 +0530

    Calculate: Total Amount

--- src/cart.js
+++ src/cart.js
@@ -89,11 +89,30 @@ let update = (id) => {
   console.log(search.item);
   document.getElementById(id).innerHTML = search.item;
   calculation();
+  totalAmount();
 };
 
 let removeItem = (id) => {
   let selectedItem = id;
   basket = basket.filter((x) => x.id !== selectedItem.id);
   generateCartItems();
+  totalAmount();
   localStorage.setItem("key", JSON.stringify(basket));
 };
+
+let totalAmount = () => {
+  if (basket.length !== 0) {
+    let amount = basket
+      .map((x) => {
+        let { item, id } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+        return item * search.price;
+      })
+      .reduce((x, y) => x + y, 0);
+
+    label.innerHTML = `<h2>Total Bill: <i class="bi bi-currency-rupee">${amount}</i></h2>
+    <button class='checkout'> Checkout</button>
+    <button class='removeAll'> Clear Cart</button>`;
+  } else return;
+};
+totalAmount();
